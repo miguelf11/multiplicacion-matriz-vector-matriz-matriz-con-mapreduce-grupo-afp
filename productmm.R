45aa3a2d2aec1acea14131c72bef1e1e0088e63e
Miguel Figueira <miguelachof11@gmail.com>
2016-03-21 11:49:40 -0430

    V2

--- productmm.R
+++ productmm.R
@@ -5,11 +5,9 @@ productmm <- function(matriz,matriz2,n,limite = -1){
   con.matriz2 <- file(vector, "r")
   
   memoria.limite <- memlimit(limite)
-  #saltar el header del csv , de prueba no lo tendrá 
-  readLines(con.matriz,1)
-  #saltar el header del csv , de prueba no lo tendrá 
-  readLines(con.matriz2,1)
   
+  #readLines(con.matriz,1)
+  #readLines(con.matriz2,1)
   
   close(con.matriz,type="r")
   close(con.matriz2,type="r")
--- proyecto.R
+++ proyecto.R
@@ -1,77 +1,83 @@
-# este script es principalmente un borrador para ir probando
-# las funciones.
+#probando y creando producto entre matriz y vector
 
-productmv <- function(matriz,vector,n,limite = -1 ){
-  con.matriz <- file(matriz, "r")
-  con.vector <- file(vector, "r")
-  close(con.matriz,type="r")
-  close(con.vector,type="r")
-  if (limite != -1){
-    
+# library("parallel") sirve para usar más de un "core"
+
+vxv <- function (vector1,vector2){
+  #vector 1 es el vector de la matriz
+  #vector 2 es el vector
+  sum <- 0
+  for (i in 1:length(vector1)){
+    sum <- vector1[i] * vector2[i] + sum
   }
+  return (sum) 
 }
 
+matriz <- "tblAkv3x3.csv"
+vector <- "tblxkv3.csv"
+
+
+limite <- -1
+n <- 3
+
+
+con.matriz <- file(matriz, "r")
+con.vector <- file(vector, "r")
+
+tam.matriz <- object.size(readLines(con.matriz,1))
+
+tam.vector <- object.size(readLines(con.vector,1))
+
+
+close(con.matriz,type="r")
+close(con.vector,type="r")
+
+
+
+memoria.limite <- memlimit(limite)
+
+
+#cantidad de N que puedo leer
+maximo.leer.matriz = as.integer(memoria.limite %/% tam.matriz) / 2
+
+#vuelvo a abrir la coneccion 
+con.matriz <- file(matriz, "r")
+con.vector <- file(vector, "r")
+
+
+con.resultado <- file("resultado_vector.csv", "w")
+if (maximo.leer.matriz > n){
+  # then i can read all vector and all first row of the matrix
+  # to calculate the first row of the new vector
   
-productmm <- function(matriz,matriz2,n,limite = -1){
-  # funcion que calcula multiplicacion matriz con matriz
+  #its the same vector for al the rows of the matrix
+  valores.vector <- as.numeric(unlist(strsplit(readLines(con.vector,n,warn = FALSE),",")))
+  valores.vector <- valores.vector[seq(from = 2 , to = n*2 , by = 2)]
+  for (i in 1:n){
+    valores.matriz <- as.numeric(unlist(strsplit(readLines(con.matriz,n,warn = FALSE),",")))
+    valores.matriz <- valores.matriz[seq(from = 3, to = n*3,by = 3 )]
+    c <- c(paste(i,as.character(vxv(valores.matriz,valores.vector)),sep=","))
+    print(c)
+    writeLines(c,con.resultado)
+  }
   
-  con.matriz <- file(matriz, "r")
-  readLines(con.matriz,1) #saltar el header del csv , de prueba no lo tendrá 
-  con.matriz2 <- file(vector, "r")
-  readLines(con.matriz2,1) #saltar el header del csv , de prueba no lo tendrá
-  mem <- memlimit(limit)
+}#else{
+  #for(i in 1:maximo.leer.matriz){
+   # for (j in 1:n){
+      
+    #}
+    
+  #}
   
-  close(con.matriz,type="r")
-  close(con.matriz2,type="r")
-}
   
-
-con <- file("tblAkv10x10ident.csv", "r")
-readLines(con,1) #saltar el header del csv , de prueba no lo tendrá
-terminado = TRUE
-
-#todas las lineas tendrán el mismo tamaño que el primero.
-
-a <- readLines(con,1)
-a
-
-b <- readLines(con,1)
-b
-close(con,type="r")
-object.size(b)  #104 bytes
-
-c <- gsub("\\D", " ", b)
-c
-object.size(c)  #104 bytes
-
-object.size(productmm) 
-
-vector <- unlist(strsplit(c," "))
-vector <- as.integer(vector[vector != ""])
-
-# tiene 56 bytes
-
-
-
-p <-memory.limit()
-
-
-
-# Se busca tratar de dividir las matrices en trozos que no pesen
-# mas de cierta cantidad de M cantidad de Mb, dividirlos, procesarlos y almacenar
-# solo el resultado
+#}
 
 
 
 
-# rm sirve para eliminar variables del workspace
+close(con.matriz,type="r")
+close(con.vector,type="r")
+close(con.resultado,type="w")
 
-# object.size(tblxkv10) retorna el tamaño de la variable en bytes 
 
-# memory.limit() devuelve el limite de memoria en para R en Mb(1048576 bytes)
 
-# memory.size() devuelve la cant de memoria en uso actualmente
 
-# 
-
-# funcion que calcula multiplicacion matriz con vector
