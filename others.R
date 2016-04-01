460650c8e38e92e1522b95abe3279a05bf119dd3
Miguel Figueira <miguelachof11@gmail.com>
2016-03-31 21:19:26 -0430

    delete others
    es preferible tenerlo dentro de las otras funciones y ya xD

--- others.R
+++
@@ -1,9 +0,0 @@
-vxv <- function (vector1,vector2){
-  #vector 1 es el vector de la matriz
-  #vector 2 es el vector
-  sum <- 0
-  for (i in 1:length(vector1)){
-    sum <- vector1[i] * vector2[i] + sum
-  }
-  return (sum) 
-}
